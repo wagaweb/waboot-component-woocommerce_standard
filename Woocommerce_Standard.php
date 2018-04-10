1b664911eb28e22ded19f3aa86195f910272e134
LostCore <lostcore@gmail.com>
2018-04-10 17:27:32 +0200

    Standardized version

--- Woocommerce_Standard.php
+++ Woocommerce_Standard.php
@@ -4,7 +4,7 @@ Component Name: WooCommerce Standard
 Description: An initial customization for WooCommerce
 Category: Utilities
 Tags: Woocommerce
-Version: 1.0
+Version: 1.0.0
 Author: WAGA Team <dev@waga.it>
 Author URI: http://www.waga.it
  */
